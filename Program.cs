d431ee24863e9f8edeecb26efcb58751d68dbe0f
gerel <gerel@bk.ru>
2023-12-17 14:25:55 +0500

    Add task

--- Program.cs
+++ Program.cs
@@ -1,2 +1,3 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 3: Задайте прямоугольный двумерный массив.
+//Напишите программу, которая будет находить строку
+// с наименьшей суммой элементов.
