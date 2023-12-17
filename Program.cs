48d9c6275093b1233cfb4579f8e0140520d5634b
gerel <gerel@bk.ru>
2023-12-17 14:31:44 +0500

    Add FillArray

--- Program.cs
+++ Program.cs
@@ -1,3 +1,16 @@
 ﻿// Задача 3: Задайте прямоугольный двумерный массив.
 //Напишите программу, которая будет находить строку
 // с наименьшей суммой элементов.
+
+int[,] FillArray(int rows, int columns, int min, int max)
+{
+    int[,] result = new int[rows, columns];
+    for (int i = 0; i < result.GetLength(0); i++)
+    {
+        for (int j = 0; j < result.GetLength(1); j++)
+        {
+            result[i,j] = new Random.Next(min, max);
+        }
+    }
+    return result;
+}
